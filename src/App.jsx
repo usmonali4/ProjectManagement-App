c58e2907edb9058c83934b31d7f3fb57a20572d5
Usmon <usmonpahlavonov@gmail.com>
2024-04-05 18:32:13 +0900

    Managing State to Switch Between Components and improving some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStarrAddProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject />
+  } else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStarrAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStarrAddProject}/>
+      {content}
     </main>
   );
 }
