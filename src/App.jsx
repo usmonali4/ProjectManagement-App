b3acb29300effcf2683ddbdcfc2c095943336165
Usmon <usmonpahlavonov@gmail.com>
2024-04-06 11:41:50 +0900

    managing tasks

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,33 @@ import SelectedProject from "./components/SelectedProject";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: [],
   });
+
+  function handleAddTask(text){
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text:text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      }
+    })
+  }
+
+  function handleDeleteTask(id){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id)
+      }
+    });
+  }
 
   function handleSelectProject(id){
     setProjectsState(prevState => {
@@ -63,7 +88,14 @@ function App() {
     })
   }
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  let content = 
+  <SelectedProject 
+      project={selectedProject} 
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+      />;
 
   if(projectsState.selectedProjectId === null){
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
@@ -77,6 +109,7 @@ function App() {
         onStartAddProject={handleStartAddProject} 
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
         />
       {content}
     </main>
--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,6 @@
-export default function selectedProject({project, onDelete}){
+import Tasks from "./Tasks";
+
+export default function SelectedProject({project, onDelete, onAddTask, onDeleteTask, tasks}){
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: "numeric",
         month: "short",
@@ -20,6 +22,7 @@ export default function selectedProject({project, onDelete}){
                 <p className="mb-4 text-stone-400">{formattedDate}</p>
                 <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
             </header>
+            <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks}/>
         </div>
     );
 }
