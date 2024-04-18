a0dd78944a97753bd44d57037679c322dc0ef02b
Usmon <usmonpahlavonov@gmail.com>
2024-04-18 14:21:09 +0900

    restructre; backend init

---
+++ backend/mngproject/src/main/java/com/leveUp/mngproject/MngprojectApplication.java
@@ -0,0 +1,13 @@
+package com.leveUp.mngproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MngprojectApplication.class, args);
+	}
+
+}
---
+++ backend/mngproject/src/test/java/com/leveUp/mngproject/MngprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leveUp.mngproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MngprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
